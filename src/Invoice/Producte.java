ce35862ad7e2b129e4f5cd77c5c19cf0422245fe
wendelinstadler <wendelin.lou.stadler@gmail.com>
2026-09-15 15:22:07 +0200

    Car

---
+++ src/Invoice/Producte.java
@@ -0,0 +1,15 @@
+package Invoice;
+
+public class Producte {
+    double preis;
+    double menge;
+    String name;
+
+    public Producte(double menge, double preis, String name) {
+        this.menge = menge;
+        this.preis = preis;
+        this.name = name;
+    }
+
+
+}
---
+++ src/Invoice/Rechnungspositon.java
@@ -0,0 +1,17 @@
+package Invoice;
+
+import Regatta.Ship;
+
+import java.util.ArrayList;
+
+public class Rechnungspositon {
+    private ArrayList<Producte> producte;
+
+    public Rechnungspositon() {
+        this.producte = new ArrayList<>();
+    }
+
+    public void Gesamtpreis(){
+        
+    }
+}
